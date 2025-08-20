928ca217cb3f8062666178ce4dcb5a8712cce90f
Karthik G <karthikgrsk@gmail.com>
2025-08-20 17:25:16 +0000

    added Admin files

--- src/main/java/com/JobFindingPlatform/entity/User.java
+++ src/main/java/com/JobFindingPlatform/entity/User.java
@@ -25,47 +25,8 @@ public class User {
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
+    private boolean blocked;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 
 
 }
