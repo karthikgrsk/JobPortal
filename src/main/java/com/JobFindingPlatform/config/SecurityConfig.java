6c2cb740d5c17afb8cadcc871c359618ee0b05ae
Karthik G <156653445+karthikgrsk@users.noreply.github.com>
2025-10-04 17:37:11 +0530

    Update SecurityConfig.java

--- src/main/java/com/JobFindingPlatform/config/SecurityConfig.java
+++ src/main/java/com/JobFindingPlatform/config/SecurityConfig.java
@@ -56,11 +56,11 @@ class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-         configuration.setAllowedOrigins(List.of(
+         configuration.setAllowedOrigins(Arrays.asList(
         "https://ideal-halibut-7vv69wqrxpj93r675-5501.app.github.dev"
          ));
-        configuration.setAllowedHeaders(List.of(List.of("Authorization", "Content-Type")));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
